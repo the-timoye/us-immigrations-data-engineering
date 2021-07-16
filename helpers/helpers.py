4dc7672b35fb5a236f7a6fb97ba53f8a67564c80
the-timoye <thetimoye@gmail.com>
2021-07-16 11:09:09 +0100

    refactor(airflow): use airflows path

--- helpers/helpers.py
+++
@@ -1,8 +0,0 @@
-def remane_columns(df, columns_dictionary):
-    new_df = df
-    for key, value in columns_dictionary.items():
-        new_df = new_df.withColumnRenamed(key, value);
-    return new_df
-
-# def copy_data_to_s3(table, file_format, folder):
-#     table.distinct().write.mode('').file_format(f's3://bucket/{folder}')
